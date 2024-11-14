cf79e1b48988677664b97dff66d640c30b8c5095
tagrag90 <gunjeepark21@gmail.com>
2024-11-15 00:21:05 +0900

    modify profile layout

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -57,6 +57,11 @@ export default function FollowButton({
     <Button
       variant={data.isFollowedByUser ? "secondary" : "default"}
       onClick={() => mutate()}
+      className={
+        data.isFollowedByUser
+          ? "w-28 border-2 hover:bg-destructive hover:text-destructive-foreground"
+          : "w-28 bg-black text-white hover:bg-gray-800"
+      }
     >
       {data.isFollowedByUser ? "Unfollow" : "Follow"}
     </Button>
