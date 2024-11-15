8b0b7a6f26f125bfee87b73f3bfb54250a7309ba
tagrag90 <gunjeepark21@gmail.com>
2024-11-15 23:23:05 +0900

    change like icon

--- src/components/posts/LikeButton.tsx
+++ src/components/posts/LikeButton.tsx
@@ -7,7 +7,7 @@ import {
   useQuery,
   useQueryClient,
 } from "@tanstack/react-query";
-import { Heart } from "lucide-react";
+import { Flashlight, Heart, Zap } from "lucide-react";
 import { useToast } from "../ui/use-toast";
 
 interface LikeButtonProps {
@@ -60,13 +60,16 @@ export default function LikeButton({ postId, initialState }: LikeButtonProps) {
 
   return (
     <button onClick={() => mutate()} className="flex items-center gap-2">
-      <Heart
+      <Zap
         className={cn(
           "size-5",
-          data.isLikedByUser && "fill-red-500 text-red-500",
+          data.isLikedByUser && "fill-[#00dd89] text-[#00dd89]",
         )}
       />
-      <span className="text-sm font-medium tabular-nums">{data.likes}</span>
+      {/* <span className="text-sm font-medium tabular-nums">{data.likes}</span> */}
+      <span className="text-sm font-medium tabular-nums">
+        {data.likes} <span className="hidden sm:inline">likes</span>
+      </span>
     </button>
   );
 }
