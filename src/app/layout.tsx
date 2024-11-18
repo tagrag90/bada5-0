a4ee90963a962681c1d0fe62bbcbfdb83a08d399
tagrag90 <gunjeepark21@gmail.com>
2024-11-18 22:23:42 +0900

    add meta and opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,11 +25,41 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
+  // title: {
+  //   template: "%s | Dive to Bada",
+  //   default: "Dive to Bada",
+  // },
+  // description:
+  //   "Bada는 크리에이터와 아티스트들이 소통하고, 협업을 통해 성장할 수 있도록 돕는 소셜 미디어 커뮤니티입니다. 창작자들의 성장을 통해 다양한 콘텐츠룰 만들뿐만 아니라 사람들이 문화에 더욱 쉽게 다가가고 즐길 수 있는 기술과 문화를 만듭니다. ",
+  metadataBase: new URL("https://www.divetobada.com"), // 실제 도메인으로 변경
   title: {
-    template: "%s | Dive to Bada",
     default: "Dive to Bada",
+    template: "%s | Dive to Bada",
   },
-  description: "The social media app for Entertainment",
+  description:
+    "Bada는 크리에이터와 아티스트들이 소통하고, 협업을 통해 성장할 수 있도록 돕는 소셜 미디어 커뮤니티입니다. 창작자들의 성장을 통해 다양한 콘텐츠룰 만들뿐만 아니라 사람들이 문화에 더욱 쉽게 다가가고 즐길 수 있는 기술과 문화를 만듭니다. ",
+  openGraph: {
+    title: "Dive to Bada",
+    description: "크리에이터와 아티스트들의 소셜 미디어 커뮤니티",
+    url: "https://www.divetobada.com",
+    siteName: "Dive to Bada",
+    images: [
+      {
+        url: "/og-image.png", // 공유될 때 표시될 이미지
+        width: 1200,
+        height: 630,
+        alt: "Dive to Bada",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Dive to Bada",
+    description: "크리에이터와 아티스트들의 소셜 미디어 커뮤니티",
+    images: ["/og-image.png"],
+  },
 };
 
 export const viewport: Viewport = {
