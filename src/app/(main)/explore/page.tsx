bf208dfc73382de6927d1702c989a80d41b3ad82
gunjee <gunjeepark21@gmail.com>
2025-01-17 00:01:42 +0900

    Menubar

--- src/app/(main)/explore/page.tsx
+++ src/app/(main)/explore/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="flex w-full min-w-0 gap-5">
-      <div className="w-full min-w-0 space-y-0 p-5">
+      <div className="w-full min-w-0 space-y-0 mx-2">
         {/* <div className="rounded-2xl bg-card p-5 shadow-sm md:hidden">
           <h1 className="text-center text-2xl font-bold">탐험</h1>
         </div> */}
